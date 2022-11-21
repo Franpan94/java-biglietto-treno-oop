0d837f28594395ea26e4d51ed1731b9f28bb3cd5
Franpan94 <francescopanico94@libero.it>
2022-11-21 19:01:59 +0100

    new commit

--- src/CalcolaBigliettoTrenoOop/Biglietteria.java
+++ src/CalcolaBigliettoTrenoOop/Biglietteria.java
@@ -1,26 +1,36 @@
 package CalcolaBigliettoTrenoOop;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Biglietteria {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-    	System.out.println("Start");
     	
     	System.out.print("Inserisci età: ");
-    	int age = sc.nextInt();
+    	String ageStr = sc.nextLine();
+    	int age = Integer.parseInt(ageStr);
     	
     	System.out.print("Inserisci Km: ");
-    	int km = sc.nextInt();
+    	String kmStr = sc.nextLine();
+    	int km = Integer.parseInt(kmStr);
     	
-		try {
-			Biglietto b = new Biglietto(age, km);
+    	System.out.print("Inserisci opzione se flessibile o meno(rispondi con si o no): ");
+    	String isFlexibleStr = sc.nextLine();
+    	boolean isFlexible = isFlexibleStr.equals("si");
+    	
+    	LocalDate dateOfDay = LocalDate.now();
+    	
+    	System.out.println("--------------------------------------------------");
+    	
+    	try {
+			Biglietto b = new Biglietto(age, km, dateOfDay, isFlexible);
 			System.out.println(b);
 			
 		}catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-		System.out.println("End");
+		
 	}
 	   
 }
--- src/CalcolaBigliettoTrenoOop/Biglietto.java
+++ src/CalcolaBigliettoTrenoOop/Biglietto.java
@@ -1,31 +1,39 @@
 package CalcolaBigliettoTrenoOop;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Biglietto {
     private int km;
 	private int age;
-	private final static BigDecimal COSTO_KM = new BigDecimal(0.21);
-	private final static BigDecimal DISCOUNT_OVER_65 = new BigDecimal (0.04);
-	private final static BigDecimal DISCOUNT_UNDER_18 = new BigDecimal (0.02);
+	private final static BigDecimal COSTO_KM = new BigDecimal(.21);
+	private final static BigDecimal DISCOUNT_OVER_65 = new BigDecimal (.4);
+	private final static BigDecimal DISCOUNT_UNDER_18 = new BigDecimal (.2);
+	private LocalDate localDate;
+	private boolean isFlexible;
+	private final static int NORMAL_DURATION = 30;
+	private final static int FLEXIBLE_DURATION = 90;
 	
-	public Biglietto(int age, int km) throws Exception {
-		this.km = km;
-		this.age = age;
-		setAge(age);
-		setKm(km);
-		
+	public Biglietto(int age, int km, LocalDate localDate, boolean isFlexible) throws Exception {
+		isValidAge(age);
+		isValidKm(km);
+		setFlexible(isFlexible);
+		setLocalDate(localDate);
 	}
 
 	public int getKm() {
 		return km;
 	}
 
-	public void setKm(int km) throws Exception {
-		if(km <= 0) {
-			throw new Exception("Il numero di Km deve essere maggiore di 0");
-		}
+	public void setKm(int km){
 		this.km = km;
+	}
+	
+	public void isValidKm(int km) throws Exception{
+		if(km <= 0) {
+			throw new Exception("I km devono essere di un numero maggiore di 0");
+		}
+		setKm(km);
 	}
 
 	public int getAge() {
@@ -33,10 +41,14 @@ public class Biglietto {
 	}
 
 	public void setAge(int age) throws Exception {
+		this.age = age;
+	}
+	
+	public void isValidAge(int age) throws Exception {
 		if(age <= 0) {
 			throw new Exception("L'età deve essere maggiore di 0");
 		}
-		this.age = age;
+		setAge(age);
 	}
 	
 	public BigDecimal calculatePrice() {
@@ -48,11 +60,17 @@ public class Biglietto {
 	private BigDecimal calculateDiscount(BigDecimal priceDiscount) {
 		if(age < 18) {
 			priceDiscount = priceDiscount.subtract(priceDiscount.multiply(DISCOUNT_UNDER_18));
+			if(isFlexible) {
+				return priceDiscount = priceDiscount.add(priceDiscount.multiply(BigDecimal.valueOf(.1)));
+            }
 			return priceDiscount;
 		}
 			
 		else if(age > 65) {
 			priceDiscount = priceDiscount.subtract(priceDiscount.multiply(DISCOUNT_OVER_65));
+			if(isFlexible) {
+				return priceDiscount = priceDiscount.add( priceDiscount.multiply(BigDecimal.valueOf(.1)));
+            }
 			return priceDiscount;
 		}
 			
@@ -60,10 +78,55 @@ public class Biglietto {
 			return priceDiscount;
 	}
 	
+	public LocalDate getLocalDate() {
+		return localDate;
+	}
+
+	public void setLocalDate(LocalDate localDate) {
+		this.localDate = localDate;
+	}
 	
+	public LocalDate calculateExpiryDate() {
+		if(isFlexible) {
+			return localDate.plusDays(FLEXIBLE_DURATION);
+		} else {
+			return localDate.plusDays(NORMAL_DURATION);
+		}
+	}
+
+	public boolean isFlexible() {
+		return isFlexible;
+	}
+
+	public void setFlexible(boolean isFlexible) {
+		this.isFlexible = isFlexible;
+	}
+
+	public static BigDecimal getCostoKm() {
+		return COSTO_KM;
+	}
+
+	public static BigDecimal getDiscountOver65() {
+		return DISCOUNT_OVER_65;
+	}
+
+	public static BigDecimal getDiscountUnder18() {
+		return DISCOUNT_UNDER_18;
+	}
+
+	public static int getNormalDuration() {
+		return NORMAL_DURATION;
+	}
+
+	public static int getFlexibleDuration() {
+		return FLEXIBLE_DURATION;
+	}
+
 	@Override
 	public String toString() {
 		return "Età del passeggero: " + age +  "\nKm inseriti: " + km 
-				+ "\nPrezzo biglietto: " + calculatePrice() + " €";
+				+ "\nPrezzo biglietto: " + String.format("%.2f", calculatePrice()) + " €" 
+				+ "\nData odierna: " + getLocalDate() + "\nBiglietto flessibile: " + (isFlexible() ? "si" : "no")
+				+ "\nData di scadenza: " + calculateExpiryDate();
 	}
 }
